8e27f6bdd20b21303d0970416981807bb46533a8
Azimjon Abdugafforov <azimjonabdugafforov17@gmail.com>
2024-05-17 11:07:24 +0500

    minor changes done

--- src/plugins/router/routes.js
+++ src/plugins/router/routes.js
@@ -15,10 +15,10 @@ export const routes = [
         component: () => import('@/pages/account-settings.vue'),
       },
       {
-        path: 'user-profile' ,
+        path: 'user-profile',
         name: 'user-profile',
-        component: ()=> import('@/pages/user-profile.vue')
-      }
+        component: () => import('@/pages/user-profile.vue'),
+      },
     ],
   },
   {
