26adf778459be4685dcc8d0d9c482bb1e6616449
Mirafzal0713 <usmonjonovmirafzal0713@gmail.com>
2024-06-06 12:58:52 +0500

    some fixes

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,9 +1,8 @@
 <script setup>
+import { useBranchStore } from '@/store/branch';
 import { useEmployeeStore } from '@/store/employee';
-import TeamTable from '@/views/dashboard/TeamTable.vue';
 import UserProfileCard from '@/views/dashboard/UserProfileCard.vue';
 import { storeToRefs } from 'pinia';
-import { useBranchStore } from '@/store/branch';
 import { onMounted, ref } from 'vue';
 
 const branchStore = useBranchStore()
@@ -15,14 +14,15 @@ const useEmployee = useEmployeeStore();
 const { employeeList, period, employee_KPI } = storeToRefs(useEmployee);
 const { getEmployeeList, getPeriodList, getEmployeeKpiById } = useEmployeeStore()
 
+const activeItem = ref(null);
+
 
 const headers = ref([
   { title: 'ID', key: 'emp_id' },
   { title: 'F.I.SH', key: 'full_name' },
   { title: 'Tarkibiy bo\'linma nomi', key: 'structural_name' },
   { title: 'Lavozimi', key: 'position' },
-  { title: 'KPI', key: 'kpi' },
-  { title: 'O\'rtacha KPI', key: 'average_kpi' },
+
 ]);
 
 const filters = ref({
@@ -40,11 +40,6 @@ onMounted(() => {
 })
 
 
-
-
-
-
-
 const itemsPerPage = ref(filters.value.size);
 // const totalItems = ref(items.value.length);
 
@@ -60,14 +55,22 @@ const applyFilters = () => {
   getEmployeeList(filters.value.page, filters.value.size, filters.value.branch, filters.value.param, filters.value.period);
 };
 
-const getEmployeeKPI_ByID = async (id) => {
-
-  await getEmployeeKpiById(id, '2024-03-30');
+const getEmployeeKPI_ByID = async (item) => {
+  employeeList.value.items.forEach(element => {
+    element.isActive = false;
+  });
+  item.isActive = true;
+  await getEmployeeKpiById(item.emp_id, '2024-03-30');
 
 }
 
 function formatNumberRoundDown(num) {
   return Math.floor(num * 100) / 100;
+};
+
+function getTableClass(item) {
+  if (item.isActive)
+    return 'active-item'
 };
 
 
@@ -83,9 +86,10 @@ function formatNumberRoundDown(num) {
               <v-text-field v-model="filters.param" label="Qidirish"></v-text-field>
             </v-col>
             <v-col cols="12" md="3">
-              <v-autocomplete v-model="filters.branch" :items="allBranches" label="Filial" item-title="branch_name"
-                item-value="id"></v-autocomplete>
+              <v-autocomplete v-model="filters.branch" :items="allBranches" label="Filial"
+                item-title="branch_name" item-value="id"></v-autocomplete>
             </v-col>
+      
             <v-col cols="12" md="3">
               <v-autocomplete v-model="filters.period" :items="period" item-title="period" item-value="id"
                 label="Davr" />
@@ -97,138 +101,112 @@ function formatNumberRoundDown(num) {
         </v-container>
       </v-card>
     </VCol>
-
-    <VCol cols="12" md="9" class="table-height" >
-      <v-card class="pa-4 border h-100">
-        <VDataTable :headers="headers" :items="employeeList.items" :items-per-page="itemsPerPage"
-          @update:items-per-page="updateItemsPerPage" item-value="id" class="text-no-wrap h-100 overflow-y-auto"
-          :items-length="employeeList.total">
-          <template v-slot:item="{ item }">
-            <tr>
-              <td @click="getEmployeeKPI_ByID(item.emp_id)" class="cursor-pointer">
-                {{ item.emp_id }}
-              </td>
-              <td @click="getEmployeeKPI_ByID(item.emp_id)" class="cursor-pointer">
-                {{ item.full_name }}
-              </td>
-              <td @click="getEmployeeKPI_ByID(item.emp_id)" class="cursor-pointer">
-                {{ item.structural_name }}
-              </td>
-              <td @click="getEmployeeKPI_ByID(item.emp_id)" class="cursor-pointer">
-                {{ item.position }}
-              </td>
-              <td @click="getEmployeeKPI_ByID(item.emp_id)" class="cursor-pointer">
-                {{ formatNumberRoundDown(item.kpi) }}%
-              </td>
-              <td @click="getEmployeeKPI_ByID(item.emp_id)" class="cursor-pointer">
-                {{ formatNumberRoundDown(item.average_kpi) }} %
-              </td>
-            </tr>
-          </template>
-          <template #bottom >
- 
-          </template>
-        </VDataTable>
-
-      </v-card>
-    </VCol>
-    <VCol cols="12" md="3">
-      <UserProfileCard />
-    </VCol>
     <VCol cols="12" class="height">
       <VCard class="border pa-4 h-100  ">
-        <VTable class="h-100 overflow-y-auto overflow-x-auto" >
+        <VTable class="h-100 overflow-y-auto overflow-x-auto">
           <thead>
             <tr>
 
               <th class="text-left">Ko'rsatkich turi</th>
-              <th class="text-left">O'rtacha KPI</th>
-              <th class="text-left">KPI Foizi</th>
+              <th class="text-left">Ko'rsatkich nomi </th>
               <th class="text-left">Reja</th>
               <th class="text-left">Fakt</th>
               <th class="text-left">Reja bajarilishi</th>
-              <th class="text-left">Ulushi</th>
-              <th class="text-left">Ko'rsatkichi </th>
-              <th class="text-left">Min</th>
-              <th class="text-left">Maks</th>
+              <th class="text-left">KPI Foizi</th>
+              <th class="text-left">Umumiy KPI</th>
 
-              <th class="text-left">Davr</th>
+
+
+
+
             </tr>
           </thead>
           <tbody>
 
-            <tr>
-              <td colspan="12">
-                <div class="d-flex ga-10 font-weight-bold">
-                  <span>
-                    F.I.SH: {{ employee_KPI.full_name }}
-                  </span> <span>
-                    Boshqarma: {{ employee_KPI.governance }}
-                  </span>
-                  <span>
-                    Lavozim: {{ employee_KPI.position }}
-                  </span>
-                  <span>
-                    KPI ko'rsatkichi: {{
-                      formatNumberRoundDown(employee_KPI.average_kpi) }}%
-                  </span>
-                </div>
 
-              </td>
-            </tr>
             <tr v-for="item in employee_KPI.kpi">
 
               <td>
                 {{ item.category }}
               </td>
               <td>
-                {{ formatNumberRoundDown(item.average_kpi) * 100 }}%
+                {{ item.indicator }}
               </td>
               <td>
-                {{ formatNumberRoundDown(item.kpi_percent) * 100 }}%
+                {{ formatNumberRoundDown(item.plan) }}
               </td>
               <td>
-                {{ formatNumberRoundDown(item.plan) }}%
-              </td>
-              <td>
-                {{ formatNumberRoundDown(item.fact) }}%
+                {{ formatNumberRoundDown(item.fact) }}
               </td>
               <td>
                 {{ formatNumberRoundDown(item.done_percent) * 100 }}%
               </td>
               <td>
-                {{ formatNumberRoundDown(item.kpi) * 100 }}%</td>
-
-              <td>
-                {{ item.indicator }}
+                {{ formatNumberRoundDown(item.kpi_percent) * 100 }}%
               </td>
               <td>
-                {{ item.min_percent * 100 }}%
-              </td>
-              <td>
-                {{
-                  item.max_percent * 100
-                }}%
-              </td>
-              <td>
-                {{
-                  item.period
-                }}
+                {{ formatNumberRoundDown(item.average_kpi) * 100 }}%
               </td>
             </tr>
           </tbody>
         </VTable>
       </VCard>
     </VCol>
+    <VCol cols="12" md="3">
+      <UserProfileCard />
+    </VCol>
+    <VCol cols="12" md="9" class="table-height">
+      <v-card class="pa-4 border h-100">
+        <VDataTable hover :headers="headers" :items="employeeList.items" :items-per-page="itemsPerPage"
+          @update:items-per-page="updateItemsPerPage" item-value="id"
+          class="text-no-wrap h-100 overflow-y-auto elevation-1" :items-length="employeeList.total">
+
+          <template v-slot:item="{ item }">
+            <tr :class="`cursor-pointer hovering-pan ${getTableClass(item)}`" @click="getEmployeeKPI_ByID(item)">
+              <td>
+                {{ item.emp_id }}
+              </td>
+              <td>
+                {{ item.full_name }}
+              </td>
+              <td>
+                {{ item.structural_name }}
+              </td>
+              <td>
+                {{ item.position }}
+              </td>
+
+            </tr>
+          </template>
+          <template #bottom>
+
+          </template>
+        </VDataTable>
+
+      </v-card>
+    </VCol>
   </VRow>
 </template>
 
 <style scoped>
 .height {
-  max-height: 215px !important;
+  max-block-size: 275px !important;
 }
 
-.table-height{
-  max-height: 450px !important;
+.table-height {
+  max-block-size: 390px !important;
+}
+
+.custom-item:hover {
+  background-color: #f0f0f0 !important;
+
+}
+
+.active-item {
+  background-color: #ccffce !important;
+}
+
+.custom-hover-effect {
+  cursor: pointer
 }
 </style>
